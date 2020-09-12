b5e10064020317f6a535874032e5b3661270d74b
David Canagasabey <david@dtcan.dev>
2020-09-12 14:55:39 -0400

    Add directory name to window title

--- src/dir.js
+++ src/dir.js
@@ -5,6 +5,7 @@ const fileList = document.getElementById("file-list");
 
 
 document.getElementById("name").innerHTML = directory.name;
+document.title = directory.name + " - " + document.title;
 
 for(let dir of directory.directories) {
     addListItem(directoryList, dir.path, dir.name);
