be752bc9843584fd877ae38c249c7b83e11c1d20
David Canagasabey <david@dtcan.dev>
2021-01-16 15:59:48 -0500

    Server checks token cookie before serving page

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const fs = require("fs");
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
 const files = require("./files");
 const auth = require("./auth");
 
 const PORT = process.env.PORT || 8080;
 
+
 const app = express();
+app.use(cookieParser());
 const encodedParser = bodyParser.urlencoded({ extended: false });
 
 app.post("/auth", encodedParser, (req, res) => {
@@ -22,62 +27,70 @@ app.post("/auth", encodedParser, (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-    fs.readFile("./dist/login.html", "utf-8", (err, data) => {
-        if(err) {
-            console.log(err);
-            res.status(500).end("Error: Could not load page");
-        }else {
-            res.setHeader('Content-Type', 'text/html');
-            res.end(data.toString().replace(/{{ root }}/g, files.getRoot()));
-        }
+    auth.checkToken(req.cookies.token).then(() => { redirectPage(res, files.getRoot()); }).catch(_ => {
+        fs.readFile("./dist/login.html", "utf-8", (err, data) => {
+            if(err) {
+                console.log(err);
+                res.status(500).end("Error: Could not load page");
+            }else {
+                res.setHeader('Content-Type', 'text/html');
+                res.end(data.toString().replace(/{{ root }}/g, files.getRoot()));
+            }
+        });
     });
 });
 
 app.get("/", (req, res) => {
-    fs.readFile("./dist/index.html", "utf-8", (err, data) => {
-        if(err) {
-            console.log(err);
-            res.status(500).end("Error: Could not load page");
-        }else {
-            files.getPaths().then(paths => {
-                res.setHeader('Content-Type', 'text/html');
-                res.end(data.toString()
-                    .replace(/{{ root }}/g, files.getRoot())
-                    .replace("{{ paths }}", JSON.stringify(paths).replace(/"/g, "\\\"")));
-            }).catch(err => {
-                res.status(err.statusCode || 500).end(err.message || "Unknown error");
-            });
-        }
-    });
+    auth.checkToken(req.cookies.token).then(() => {
+        fs.readFile("./dist/index.html", "utf-8", (err, data) => {
+            if(err) {
+                console.log(err);
+                res.status(500).end("Error: Could not load page");
+            }else {
+                files.getPaths().then(paths => {
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(data.toString()
+                        .replace(/{{ root }}/g, files.getRoot())
+                        .replace("{{ paths }}", JSON.stringify(paths).replace(/"/g, "\\\"")));
+                }).catch(err => {
+                    res.status(err.statusCode || 500).end(err.message || "Unknown error");
+                });
+            }
+        });
+    }).catch(_ => { redirectPage(res, files.getRoot()+"login"); });
 });
 
 app.get("/dir/*", (req, res) => {
-    fs.readFile("./dist/dir.html", "utf-8", (err, data) => {
-        if(err) {
-            console.log(err);
-            res.setHeader('Content-Type', 'text/plain');
-            res.status(500).end("Error: Could not load page");
-        }else {
-            files.getDirectory(req.path).then(dir => {
-                res.setHeader('Content-Type', 'text/html');
-                res.end(data.toString()
-                    .replace(/{{ root }}/g, files.getRoot())
-                    .replace("{{ dir }}", JSON.stringify(dir).replace(/"/g, "\\\"")));
-            }).catch(err => {
-                res.status(err.statusCode || 500).end(err.message || "Unknown error");
-            });
-        }
-    });
+    auth.checkToken(req.cookies.token).then(() => {
+        fs.readFile("./dist/dir.html", "utf-8", (err, data) => {
+            if(err) {
+                console.log(err);
+                res.setHeader('Content-Type', 'text/plain');
+                res.status(500).end("Error: Could not load page");
+            }else {
+                files.getDirectory(req.path).then(dir => {
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(data.toString()
+                        .replace(/{{ root }}/g, files.getRoot())
+                        .replace("{{ dir }}", JSON.stringify(dir).replace(/"/g, "\\\"")));
+                }).catch(err => {
+                    res.status(err.statusCode || 500).end(err.message || "Unknown error");
+                });
+            }
+        });
+    }).catch(_ => { redirectPage(res, files.getRoot()+"login"); });
 });
 
 app.get("/file/*", (req, res) => {
-    files.getFile(req.path, req.query.thumb != undefined).then(file => {
-        res.setHeader('Content-Type', file.type);
-        res.end(file.data);
-    }).catch(err => {
-        res.setHeader('Content-Type', 'text/plain');
-        res.status(err.statusCode || 500).end(err.message || "Unknown error");
-    });
+    auth.checkToken(req.cookies.token).then(() => {
+        files.getFile(req.path, req.query.thumb != undefined).then(file => {
+            res.setHeader('Content-Type', file.type);
+            res.end(file.data);
+        }).catch(err => {
+            res.setHeader('Content-Type', 'text/plain');
+            res.status(err.statusCode || 500).end(err.message || "Unknown error");
+        });
+    }).catch(_ => { redirectPage(res, files.getRoot()+"login"); });
 });
 
 app.get("/*.js", (req, res) => {
@@ -93,3 +106,21 @@ app.get("/*.js", (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+
+function redirectPage(res, url) {
+    fs.readFile("./dist/redirect.html", "utf-8", (err, data) => {
+        if(err) {
+            console.log(err);
+            res.status(500).end("Error: Could not load page");
+        }else {
+            files.getPaths().then(_ => {
+                res.setHeader('Content-Type', 'text/html');
+                res.end(data.toString()
+                    .replace(/{{ url }}/g, url));
+            }).catch(err => {
+                res.status(err.statusCode || 500).end(err.message || "Unknown error");
+            });
+        }
+    });
+}
